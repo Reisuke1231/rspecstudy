2d2cb3aae08940a77cc59043fed8de3dff3111e4
Reisuke1231 <reisuke1231@gmail.com>
2022-03-31 13:01:15 +0900

    Skip rspec

--- car_project/spec/car_spec.rb
+++ car_project/spec/car_spec.rb
@@ -4,7 +4,7 @@ describe 'Car' do
 
     describe 'attributes' do
 
-        it "allows reading and writing for :make" do
+        xit "allows reading and writing for :make" do
             car = Car.new
             car.make = 'Test'
             expect(car.make).to eq('Test')
